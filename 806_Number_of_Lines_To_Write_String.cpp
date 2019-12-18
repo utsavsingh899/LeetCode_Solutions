7f04afadaa0c36cc3523a07f8b1c7ea4d99d3088
us899 <utsavsingh899@gmail.com>
2019-12-19 02:07:28 +0530

    Added 806

---
+++ 806_Number_of_Lines_To_Write_String.cpp
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> numberOfLines(vector<int>& widths, string S) {
+        vector<int> ans(2);
+        for (int i = 0; i < S.size(); ++i) {
+            if ((ans[1] + widths[S[i] - 'a']) > 100) {
+                ans[1] = widths[S[i] - 'a'];
+                ++ans[0];
+            }
+            else 
+                ans[1] += widths[S[i] - 'a'];
+        }
+        if(ans[0] != 0 || ans[1] != 0)
+            ++ans[0];
+        return ans;
+    }
+};
+
+
+
+
