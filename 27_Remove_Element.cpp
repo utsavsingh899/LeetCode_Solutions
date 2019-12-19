2b91d39b768f92ce9e3d02371e719fb80093f2ba
us899 <utsavsingh899@gmail.com>
2019-12-19 22:37:14 +0530

    Updated 283 and 27

--- 27_Remove_Element.cpp
+++ 27_Remove_Element.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int newlen = nums.size(), i, j;
-        for (i = 0; i < nums.size(); ++i) {
+        int i = 0, n = nums.size();
+        while(i < n) {
             if (nums[i] == val) {
-                nums[i] = -1;
-                --newlen;
+                nums[i] = nums[n - 1];
+                --n;
             }
+            else
+                ++i;
         }
-        for (i = 0; i < newlen; ++i) {
-            if (nums[i] == -1)
-                for (j = i + 1; j < nums.size(); ++j) {
-                    if (nums[j] != -1) {
-                        swap(nums[i], nums[j]);
-                        break;
-                    }
-                }
-        }
-        return newlen;
+        return n;
     }
 };
--- 283_Move_Zeroes.cpp
+++ 283_Move_Zeroes.cpp
@@ -1,27 +1,9 @@
-
-
-
-
-
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i, j, new_len = nums.size();
-        for (i = 0; i < nums.size(); ++i) 
-            if (nums[i] == 0)
-                --new_len;
-        for (i = 0; i < new_len; ++i) {
-            if (nums[i] == 0) {
-                for (j = i+1; j < nums.size(); ++j) {
-                    if (nums[j] != 0) {
-                        swap(nums[i], nums[j]);
-                        break;
-                    }
-                }
-            }
+        for (int curr = 0, lastNonZero = 0; curr < nums.size(); ++curr) {
+            if (nums[curr] != 0)
+                swap(nums[lastNonZero++], nums[curr]);
         }
     }
 };
-
-
-
