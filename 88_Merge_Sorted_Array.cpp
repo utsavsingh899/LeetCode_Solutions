3a124de8bf057a1244ffae6ef6a9ad6c3aa4284b
Utsav Singh <utsavsingh899@gmail.com>
2020-08-31 11:04:25 +0530

    Added 88, 101 & 222

---
+++ 88_Merge_Sorted_Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums2[j] > nums1[i]) {
+                nums1[k] = nums2[j];
+                --j;
+                --k;
+            }
+            else {
+                nums1[k] = nums1[i];
+                nums1[i] = -1;
+                --k;
+                --i;
+            }
+        }
+        while (i >= 0)
+            nums1[k--] = nums1[i--];
+        while (j >= 0)
+            nums1[k--] = nums2[j--];
+        return;
+    }
+};
