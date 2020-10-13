3d796872c4895ea8b16cf12f439b7124692c53dc
Utsav Singh <utsavsingh899@gmail.com>
2020-10-13 22:17:37 +0530

    Add 153 & 235

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,24 @@
+
+
+
+
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int lo = 0, hi = nums.size() - 1, mid;
+        
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            if (nums[mid] > nums[hi])
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        
+        return nums[lo];
+    }
+};
+
+
+
