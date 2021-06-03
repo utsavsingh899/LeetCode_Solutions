52b6abfb6585e5c8d0a907eae5128ec5f6fec597
Utsav Singh <utsavsingh899@gmail.com>
2021-06-04 03:35:34 +0530

    Modify 1 file

--- 238_Product_of_Array_Except_Self.cpp
+++ 238_Product_of_Array_Except_Self.cpp
@@ -1,24 +1,19 @@
-
-
-
-
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> pre(n, 1), suff(n, 1);
-
-        for (int i = 1; i < n; ++i)
-            pre[i] = pre[i - 1] * nums[i - 1];
-
-        for (int i = n - 2; i >= 0; --i)
-            suff[i] = suff[i + 1] * nums[i + 1];
-
-        for (int i = 0; i < n; ++i)
-            nums[i] = pre[i] * suff[i];
-
-        return nums;
+        
+        int left = 1, right = 1;
+        
+        vector<int> res(n, 1);
+        for (int i = 0; i < n; ++i) {
+            res[i] *= left;
+            left *= nums[i];
+            
+            res[n - i - 1] *= right;
+            right *= nums[n - 1 - i];
+        }
+        
+        return res;
     }
 };
-
-
